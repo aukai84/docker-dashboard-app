d373bc4beb06616804754f95693c1e107f06d743
Aukai Tirrell <aukai_tirrell@datahouse.com>
2018-09-17 20:05:02 -1000

    updated render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 import App from './containers/App.js';
 
 render(<App />, document.getElementById('app'));
