a4a4806d8e420d43c5428ee874a0200002489d64
Aukai Tirrell <aukai_tirrell@datahouse.com>
2018-09-17 22:26:55 -1000

    using material ui for react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
 
 import App from './containers/App.js';
 
