980ec751c9c7a5e544e851dc3f1b911d14354337
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-19 13:22:30 -1000

    wrapping charts in card component

--- src/components/views/dashboard.js
+++ src/components/views/dashboard.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react'
-import DoughnutChart from '../doughnutChart.js'
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import DoughnutChart from '../elements/doughnutChart.js';
 
 class Dashboard extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        let data = {}
-        return (
-            <div>
-                Dashboard
-                <DoughnutChart />
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		let data = [
+			{ name: 'Group A', value: 2400 },
+			{ name: 'Group B', value: 4567 },
+			{ name: 'Group C', value: 1398 },
+			{ name: 'Group D', value: 9800 },
+			{ name: 'Group E', value: 3908 },
+			{ name: 'Group F', value: 4800 }
+		];
+		return (
+			<div>
+				Dashboard
+				<Card>
+					<DoughnutChart chartData={data} />
+				</Card>
+				<DoughnutChart chartData={data} />
+				<DoughnutChart chartData={data} />
+			</div>
+		);
+	}
 }
 
-export default Dashboard
+export default Dashboard;
