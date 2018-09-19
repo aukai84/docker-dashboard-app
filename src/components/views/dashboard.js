4fd92ccf5e7df8a39386c7bea3d9c1341c779d35
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-18 21:40:06 -1000

    router on dashboard working

---
+++ src/components/views/dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return <div>dashboard</div>;
+	}
+}
+
+export default Dashboard;
--- src/containers/navigation/mainNav.js
+++ src/containers/navigation/mainNav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
@@ -14,101 +14,101 @@ import { mailFolderListItems, otherMailFolderListItems } from './tileData';
 
 const drawerWidth = 240;
 
-const styles = (theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    appFrame: {
-        height: 440,
-        zIndex: 1,
-        overflow: 'hidden',
-        position: 'relative',
-        display: 'flex',
-        width: '100%',
-    },
-    appBar: {
-        width: `calc(100% - ${drawerWidth}px)`,
-    },
-    'appBar-left': {
-        marginLeft: drawerWidth,
-    },
-    'appBar-right': {
-        marginRight: drawerWidth,
-    },
-    drawerPaper: {
-        position: 'relative',
-        width: drawerWidth,
-    },
-    toolbar: theme.mixins.toolbar,
-    content: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.default,
-        padding: theme.spacing.unit * 3,
-    },
+const styles = theme => ({
+	root: {
+		flexGrow: 1
+	},
+	appFrame: {
+		height: 440,
+		zIndex: 1,
+		overflow: 'hidden',
+		position: 'relative',
+		display: 'flex',
+		width: '100%'
+	},
+	appBar: {
+		width: `calc(100% - ${drawerWidth}px)`
+	},
+	'appBar-left': {
+		marginLeft: drawerWidth
+	},
+	'appBar-right': {
+		marginRight: drawerWidth
+	},
+	drawerPaper: {
+		position: 'relative',
+		width: drawerWidth
+	},
+	toolbar: theme.mixins.toolbar,
+	content: {
+		flexGrow: 1,
+		backgroundColor: theme.palette.background.default,
+		padding: theme.spacing.unit * 3
+	}
 });
 
-class PermanentDrawer extends React.Component {
-    state = {
-        anchor: 'left',
-    };
+class MainNav extends Component {
+	state = {
+		anchor: 'left'
+	};
 
-    handleChange = (event) => {
-        this.setState({
-            anchor: event.target.value,
-        });
-    };
+	handleChange = event => {
+		this.setState({
+			anchor: event.target.value
+		});
+	};
 
-    render() {
-        const { classes } = this.props;
-        const { anchor } = this.state;
+	render() {
+		const { classes } = this.props;
+		const { anchor } = this.state;
 
-        const drawer = (
-            <Drawer
-                variant="permanent"
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-                anchor={anchor}
-            >
-                <div className={classes.toolbar} />
-                <Divider />
-                <List>{mailFolderListItems}</List>
-            </Drawer>
-        );
+		const drawer = (
+			<Drawer
+				variant="permanent"
+				classes={{
+					paper: classes.drawerPaper
+				}}
+				anchor={anchor}
+			>
+				<div className={classes.toolbar} />
+				<Divider />
+				<List>{mailFolderListItems}</List>
+			</Drawer>
+		);
 
-        let before = null;
-        let after = null;
+		let before = null;
+		let after = null;
 
-        if (anchor === 'left') {
-            before = drawer;
-        } else {
-            after = drawer;
-        }
+		if (anchor === 'left') {
+			before = drawer;
+		} else {
+			after = drawer;
+		}
 
-        return (
-            <div className={classes.root}>
-                <div className={classes.appFrame}>
-                    <AppBar position="absolute" className={classNames(classes.appBar, classes[`appBar-${anchor}`])}>
-                        <Toolbar>
-                            <Typography variant="title" color="inherit" noWrap>
-                                Docker Dashboard
-                            </Typography>
-                        </Toolbar>
-                    </AppBar>
-                    {before}
-                    <main className={classes.content}>
-                        <div className={classes.toolbar} />
-                        <Typography>{'You think water moves fast? You should see ice.'}</Typography>
-                    </main>
-                    {after}
-                </div>
-            </div>
-        );
-    }
+		return (
+			<div className={classes.root}>
+				<div className={classes.appFrame}>
+					<AppBar position="absolute" className={classNames(classes.appBar, classes[`appBar-${anchor}`])}>
+						<Toolbar>
+							<Typography variant="title" color="inherit" noWrap>
+								Docker Dashboard
+							</Typography>
+						</Toolbar>
+					</AppBar>
+					{before}
+					<main className={classes.content}>
+						<div className={classes.toolbar} />
+						{this.props.children}
+					</main>
+					{after}
+				</div>
+			</div>
+		);
+	}
 }
 
-PermanentDrawer.propTypes = {
-    classes: PropTypes.object.isRequired,
+MainNav.propTypes = {
+	classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(PermanentDrawer);
+export default withStyles(styles)(MainNav);
