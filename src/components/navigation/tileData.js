ecfb6181d4651ef245008f1e2f76a7944767147e
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-18 17:39:55 -1000

    prettier fixes

--- src/components/navigation/tileData.js
+++ src/components/navigation/tileData.js
@@ -13,53 +13,53 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import ReportIcon from '@material-ui/icons/Report';
 
 export const mailFolderListItems = (
-	<div>
-		<ListItem button>
-			<ListItemIcon>
-				<InboxIcon />
-			</ListItemIcon>
-			<ListItemText primary="Inbox" />
-		</ListItem>
-		<ListItem button>
-			<ListItemIcon>
-				<StarIcon />
-			</ListItemIcon>
-			<ListItemText primary="Starred" />
-		</ListItem>
-		<ListItem button>
-			<ListItemIcon>
-				<SendIcon />
-			</ListItemIcon>
-			<ListItemText primary="Send mail" />
-		</ListItem>
-		<ListItem button>
-			<ListItemIcon>
-				<DraftsIcon />
-			</ListItemIcon>
-			<ListItemText primary="Drafts" />
-		</ListItem>
-	</div>
+    <div>
+        <ListItem button>
+            <ListItemIcon>
+                <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <StarIcon />
+            </ListItemIcon>
+            <ListItemText primary="Starred" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <SendIcon />
+            </ListItemIcon>
+            <ListItemText primary="Send mail" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+        </ListItem>
+    </div>
 );
 
 export const otherMailFolderListItems = (
-	<div>
-		<ListItem button>
-			<ListItemIcon>
-				<MailIcon />
-			</ListItemIcon>
-			<ListItemText primary="All mail" />
-		</ListItem>
-		<ListItem button>
-			<ListItemIcon>
-				<DeleteIcon />
-			</ListItemIcon>
-			<ListItemText primary="Trash" />
-		</ListItem>
-		<ListItem button>
-			<ListItemIcon>
-				<ReportIcon />
-			</ListItemIcon>
-			<ListItemText primary="Spam" />
-		</ListItem>
-	</div>
+    <div>
+        <ListItem button>
+            <ListItemIcon>
+                <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary="All mail" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <DeleteIcon />
+            </ListItemIcon>
+            <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <ReportIcon />
+            </ListItemIcon>
+            <ListItemText primary="Spam" />
+        </ListItem>
+    </div>
 );
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,27 +3,27 @@ import TopNav from '../components/navigation/topNav.js';
 import axios from 'axios';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			containers: []
-		};
-	}
-	componentDidMount() {
-		axios.get('http://localhost:2375/containers/json').then(res => {
-			console.log(res);
-			this.setState({
-				containers: res.data
-			});
-		});
-	}
-	render() {
-		return (
-			<div>
-				<TopNav />
-			</div>
-		);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            containers: [],
+        };
+    }
+    componentDidMount() {
+        axios.get('http://localhost:2375/containers/json').then((res) => {
+            console.log(res);
+            this.setState({
+                containers: res.data,
+            });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <TopNav />
+            </div>
+        );
+    }
 }
 
 export default App;
