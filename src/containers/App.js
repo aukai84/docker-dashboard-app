be935ffe3f1788dab07752fca607f61c74862e0f
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-18 20:44:16 -1000

    dashboard working

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
-import TopNav from '../components/navigation/topNav.js';
+import MainNav from './navigation/mainNav.js';
 import axios from 'axios';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            containers: [],
-        };
-    }
-    componentDidMount() {
-        axios.get('http://localhost:2375/containers/json').then((res) => {
-            console.log(res);
-            this.setState({
-                containers: res.data,
-            });
-        });
-    }
-    render() {
-        return (
-            <div>
-                <TopNav />
-            </div>
-        );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			containers: []
+		};
+	}
+	componentDidMount() {
+		axios.get('http://localhost:2375/containers/json').then(res => {
+			console.log(res);
+			this.setState({
+				containers: res.data
+			});
+		});
+	}
+	render() {
+		return (
+			<div>
+				<MainNav />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/containers/navigation/tileData.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Dashboard from '@material-ui/icons/Dashboard';
+import DataUsage from '@material-ui/icons/DataUsage';
+import WebAsset from '@material-ui/icons/WebAsset';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ReportIcon from '@material-ui/icons/Report';
+import NetworkCell from '@material-ui/icons/NetworkCell';
+import Album from '@material-ui/icons/Album';
+
+export const mailFolderListItems = (
+	<div>
+		<ListItem button>
+			<ListItemIcon>
+				<Dashboard />
+			</ListItemIcon>
+			<ListItemText primary="Dashboard" />
+		</ListItem>
+		<ListItem button>
+			<ListItemIcon>
+				<WebAsset />
+			</ListItemIcon>
+			<ListItemText primary="Containers" />
+		</ListItem>
+		<ListItem button>
+			<ListItemIcon>
+				<Album />
+			</ListItemIcon>
+			<ListItemText primary="Images" />
+		</ListItem>
+		<ListItem button>
+			<ListItemIcon>
+				<DataUsage />
+			</ListItemIcon>
+			<ListItemText primary="Usage" />
+		</ListItem>
+		<ListItem button>
+			<ListItemIcon>
+				<NetworkCell />
+			</ListItemIcon>
+			<ListItemText primary="Networks" />
+		</ListItem>
+	</div>
+);
