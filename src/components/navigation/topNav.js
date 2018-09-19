ccc90a438b0b5d8b162afea73ecd03aba8adaf27
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-18 17:39:42 -1000

    babel presets working, navigation drawer working

--- src/components/navigation/topNav.js
+++ src/components/navigation/topNav.js
@@ -16,138 +16,138 @@ import { mailFolderListItems, otherMailFolderListItems } from './tileData';
 
 const drawerWidth = 240;
 
-const styles = (theme) => ({
-    root: {
-        flexGrow: 1,
-        height: 440,
-        zIndex: 1,
-        overflow: 'hidden',
-        position: 'relative',
-        display: 'flex',
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginLeft: 12,
-        marginRight: 36,
-    },
-    hide: {
-        display: 'none',
-    },
-    drawerPaper: {
-        position: 'relative',
-        whiteSpace: 'nowrap',
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerPaperClose: {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing.unit * 7,
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing.unit * 9,
-        },
-    },
-    toolbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: '0 8px',
-    },
-    content: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.default,
-        padding: theme.spacing.unit * 3,
-    },
+const styles = theme => ({
+	root: {
+		flexGrow: 1,
+		height: 440,
+		zIndex: 1,
+		overflow: 'hidden',
+		position: 'relative',
+		display: 'flex'
+	},
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		})
+	},
+	appBarShift: {
+		marginLeft: drawerWidth,
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen
+		})
+	},
+	menuButton: {
+		marginLeft: 12,
+		marginRight: 36
+	},
+	hide: {
+		display: 'none'
+	},
+	drawerPaper: {
+		position: 'relative',
+		whiteSpace: 'nowrap',
+		width: drawerWidth,
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen
+		})
+	},
+	drawerPaperClose: {
+		overflowX: 'hidden',
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		}),
+		width: theme.spacing.unit * 7,
+		[theme.breakpoints.up('sm')]: {
+			width: theme.spacing.unit * 9
+		}
+	},
+	toolbar: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		padding: '0 8px'
+	},
+	content: {
+		flexGrow: 1,
+		backgroundColor: theme.palette.background.default,
+		padding: theme.spacing.unit * 3
+	}
 });
 
 class MiniDrawer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false,
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			open: false
+		};
+	}
 
-    handleDrawerOpen = () => {
-        this.setState({ open: true });
-    };
+	handleDrawerOpen = () => {
+		this.setState({ open: true });
+	};
 
-    handleDrawerClose = () => {
-        this.setState({ open: false });
-    };
+	handleDrawerClose = () => {
+		this.setState({ open: false });
+	};
 
-    render() {
-        const { classes, theme } = this.props;
+	render() {
+		const { classes, theme } = this.props;
 
-        return (
-            <div className={classes.root}>
-                <AppBar
-                    position="absolute"
-                    className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
-                >
-                    <Toolbar disableGutters={!this.state.open}>
-                        <IconButton
-                            color="inherit"
-                            aria-label="Open drawer"
-                            onClick={this.handleDrawerOpen}
-                            className={classNames(classes.menuButton, this.state.open && classes.hide)}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="title" color="inherit" noWrap>
-                            Mini variant drawer
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    variant="permanent"
-                    classes={{
-                        paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
-                    }}
-                    open={this.state.open}
-                >
-                    <div className={classes.toolbar}>
-                        <IconButton onClick={this.handleDrawerClose}>
-                            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-                        </IconButton>
-                    </div>
-                    <Divider />
-                    <List>{mailFolderListItems}</List>
-                    <Divider />
-                    <List>{otherMailFolderListItems}</List>
-                </Drawer>
-                <main className={classes.content}>
-                    <div className={classes.toolbar} />
-                    <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
-                </main>
-            </div>
-        );
-    }
+		return (
+			<div className={classes.root}>
+				<AppBar
+					position="absolute"
+					className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
+				>
+					<Toolbar disableGutters={!this.state.open}>
+						<IconButton
+							color="inherit"
+							aria-label="Open drawer"
+							onClick={this.handleDrawerOpen}
+							className={classNames(classes.menuButton, this.state.open && classes.hide)}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="title" color="inherit" noWrap>
+							Docker Dashboard
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<Drawer
+					variant="permanent"
+					classes={{
+						paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose)
+					}}
+					open={this.state.open}
+				>
+					<div className={classes.toolbar}>
+						<IconButton onClick={this.handleDrawerClose}>
+							{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+						</IconButton>
+					</div>
+					<Divider />
+					<List>{mailFolderListItems}</List>
+					<Divider />
+					<List>{otherMailFolderListItems}</List>
+				</Drawer>
+				<main className={classes.content}>
+					<div className={classes.toolbar} />
+					<Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+				</main>
+			</div>
+		);
+	}
 }
 
 MiniDrawer.propTypes = {
-    classes: PropTypes.object.isRequired,
-    theme: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
+	theme: PropTypes.object.isRequired
 };
 
 export default withStyles(styles, { withTheme: true })(MiniDrawer);
