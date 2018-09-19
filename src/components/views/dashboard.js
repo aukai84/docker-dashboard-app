4158a8a130b850b22f06b02e2549ab646fa8e094
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-19 01:45:36 -1000

    doughnut chart rendering

--- src/components/views/dashboard.js
+++ src/components/views/dashboard.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import DoughnutChart from '../doughnutChart.js';
 
 class Dashboard extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return <div>dashboard</div>
-    }
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		let data = {};
+		return (
+			<div>
+				Dashboard
+				<DoughnutChart />
+			</div>
+		);
+	}
 }
 
-export default Dashboard
+export default Dashboard;
