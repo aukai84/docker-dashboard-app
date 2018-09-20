f0e5f7dfa45fbcb433075584ecc0ec9d62d31513
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-19 23:48:45 -1000

    added networks to dashboard

--- src/containers/mainView.js
+++ src/containers/mainView.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Dashboard from '../components/views/dashboard.js'
-import Containers from '../components/views/containers.js'
-import Images from '../components/views/images.js'
-import Usage from '../components/views/usage.js'
-import Networks from '../components/views/networks.js'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from '../components/views/dashboard.js';
+import Containers from '../components/views/containers.js';
+import Images from '../components/views/images.js';
+import Usage from '../components/views/usage.js';
+import Networks from '../components/views/networks.js';
 
 class MainView extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (
-            <Switch>
-                <Route path="/" exact render={() => <Dashboard {...this.props} />} />
-                <Route path="/containers" render={() => <Containers containers={this.props.containers} />} />
-                <Route path="/images" render={() => <Images images={this.props.images} />} />
-                <Route path="/usage" component={Usage} />
-                <Route path="/networks" component={Networks} />
-            </Switch>
-        )
-    }
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<Switch>
+				<Route path="/" exact render={() => <Dashboard {...this.props} />} />
+				<Route path="/containers" render={() => <Containers containers={this.props.containers} />} />
+				<Route path="/images" render={() => <Images images={this.props.images} />} />
+				<Route path="/usage" component={Usage} />
+				<Route path="/networks" render={() => <Networks networks={this.props.networks} />} />
+			</Switch>
+		);
+	}
 }
 
-export default MainView
+export default MainView;
