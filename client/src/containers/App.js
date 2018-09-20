ffda4b64dbab70ef3e540386505f8a28da39b3f7
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-20 10:54:23 -1000

    updating file structure, adding express server

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -3,7 +3,14 @@ import MainNav from './navigation/mainNav.js'
 import axios from 'axios'
 import io from 'socket.io-client'
 import MainView from './mainView.js'
+const Docker = require('dockerode')
+const docker = new Docker({
+    socketPath: '/var/run/docker.sock',
+    host: 'http://unix:/var/run/docker.sock',
+    port: 2375,
+})
 
+console.log('process', process.env)
 class App extends Component {
     constructor(props) {
         super(props)
@@ -11,9 +18,13 @@ class App extends Component {
             containers: [],
             images: [],
             networks: [],
+            dockerContainers: [],
         }
     }
     componentDidMount() {
+        docker.listContainers((err, containers) => {
+            console.log(containers)
+        })
         axios.get('http://localhost:2375/images/json').then((res) => {
             this.setState({
                 images: res.data,
@@ -31,7 +42,7 @@ class App extends Component {
         })
     }
     render() {
-        console.log('app state', this.state)
+        console.log('docker', this.state)
         return (
             <MainNav>
                 <MainView
