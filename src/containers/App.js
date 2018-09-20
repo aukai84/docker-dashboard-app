69f055dec8ce66aa9c516d7cb32a7e891706d0eb
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-20 01:12:36 -1000

    prettier update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
-import MainNav from './navigation/mainNav.js';
-import axios from 'axios';
-import io from 'socket.io-client';
-import MainView from './mainView.js';
+import React, { Component } from 'react'
+import MainNav from './navigation/mainNav.js'
+import axios from 'axios'
+import io from 'socket.io-client'
+import MainView from './mainView.js'
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			containers: [],
-			images: [],
-			networks: []
-		};
-	}
-	componentDidMount() {
-		axios.get('http://localhost:2375/images/json').then(res => {
-			this.setState({
-				images: res.data
-			});
-		});
-		axios.get('http://localhost:2375/containers/json').then(res => {
-			this.setState({
-				containers: res.data
-			});
-		});
-		axios.get('http://localhost:2375/networks').then(res => {
-			this.setState({
-				networks: res.data
-			});
-		});
-	}
-	render() {
-		console.log('app state', this.state);
-		return (
-			<MainNav>
-				<MainView
-					containers={this.state.containers}
-					images={this.state.images}
-					networks={this.state.networks}
-				/>
-			</MainNav>
-		);
-	}
+    constructor(props) {
+        super(props)
+        this.state = {
+            containers: [],
+            images: [],
+            networks: [],
+        }
+    }
+    componentDidMount() {
+        axios.get('http://localhost:2375/images/json').then((res) => {
+            this.setState({
+                images: res.data,
+            })
+        })
+        axios.get('http://localhost:2375/containers/json').then((res) => {
+            this.setState({
+                containers: res.data,
+            })
+        })
+        axios.get('http://localhost:2375/networks').then((res) => {
+            this.setState({
+                networks: res.data,
+            })
+        })
+    }
+    render() {
+        console.log('app state', this.state)
+        return (
+            <MainNav>
+                <MainView
+                    containers={this.state.containers}
+                    images={this.state.images}
+                    networks={this.state.networks}
+                />
+            </MainNav>
+        )
+    }
 }
 
-export default App;
+export default App
