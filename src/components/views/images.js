ed2f9d49200fea0787367b70bd52a49778332a00
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-19 21:22:48 -1000

    prettier fixes

--- src/components/views/images.js
+++ src/components/views/images.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography';
-import ImagesTable from '../elements/imagesTable.js';
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core';
+import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography'
+import ImagesTable from '../elements/imagesTable.js'
+import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core'
 
 class Images extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		const headers = [
-			{ name: 'Repository/Tags', value: 'RepoTags[0]' },
-			{ name: 'Image ID', value: 'Id' },
-			{ name: 'Created', value: 'created' },
-			{ name: 'Size', value: 'size' }
-		];
-		return (
-			<div>
-				<Typography variant="headline" component="h1">
-					Images
-				</Typography>
-				<ImagesTable data={this.props.images} headers={headers} />
-			</div>
-		);
-	}
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        const headers = [
+            { name: 'Repository/Tags', value: 'RepoTags[0]' },
+            { name: 'Image ID', value: 'Id' },
+            { name: 'Created', value: 'created' },
+            { name: 'Size', value: 'size' },
+        ]
+        return (
+            <div>
+                <Typography variant="headline" component="h1">
+                    Images
+                </Typography>
+                <ImagesTable data={this.props.images} headers={headers} />
+            </div>
+        )
+    }
 }
 
-export default Images;
+export default Images
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import MainNav from './navigation/mainNav.js';
-import axios from 'axios';
-import MainView from './mainView.js';
+import React, { Component } from 'react'
+import MainNav from './navigation/mainNav.js'
+import axios from 'axios'
+import MainView from './mainView.js'
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			containers: [],
-			images: []
-		};
-	}
-	componentDidMount() {
-		axios.get('http://localhost:2375/images/json').then(res => {
-			this.setState({
-				images: res.data
-			});
-		});
-		axios.get('http://localhost:2375/containers/json').then(res => {
-			this.setState({
-				containers: res.data
-			});
-		});
-	}
-	render() {
-		return (
-			<MainNav>
-				<MainView containers={this.state.containers} images={this.state.images} />
-			</MainNav>
-		);
-	}
+    constructor(props) {
+        super(props)
+        this.state = {
+            containers: [],
+            images: [],
+        }
+    }
+    componentDidMount() {
+        axios.get('http://localhost:2375/images/json').then((res) => {
+            this.setState({
+                images: res.data,
+            })
+        })
+        axios.get('http://localhost:2375/containers/json').then((res) => {
+            this.setState({
+                containers: res.data,
+            })
+        })
+    }
+    render() {
+        return (
+            <MainNav>
+                <MainView containers={this.state.containers} images={this.state.images} />
+            </MainNav>
+        )
+    }
 }
 
-export default App;
+export default App
