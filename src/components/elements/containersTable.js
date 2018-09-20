79e33914f8baf9de0dff4fade527d29bbfb1784a
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-19 22:58:51 -1000

    addeddocker icon to table

--- src/components/elements/containersTable.js
+++ src/components/elements/containersTable.js
@@ -1,82 +1,86 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
-import Button from '@material-ui/core/Button'
-import Stop from '@material-ui/icons/Stop'
-import Delete from '@material-ui/icons/Delete'
-import AutoRenew from '@material-ui/icons/AutoRenew'
-import green from '@material-ui/core/colors/green'
-import classnames from 'classnames'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Stop from '@material-ui/icons/Stop';
+import Delete from '@material-ui/icons/Delete';
+import AutoRenew from '@material-ui/icons/AutoRenew';
+import green from '@material-ui/core/colors/green';
+import classnames from 'classnames';
+import DockerIcon from '../../assets/icons8-docker-48.png';
 
-const styles = (theme) => ({
-    root: {
-        width: '100%',
-        overflowX: 'auto',
-        marginTop: 16,
-    },
-    table: {
-        minWidth: 700,
-    },
-})
+const styles = theme => ({
+	root: {
+		width: '100%',
+		overflowX: 'auto',
+		marginTop: 16
+	},
+	table: {
+		minWidth: 700
+	}
+});
 
 class ContainersTable extends Component {
-    constructor(props) {
-        super(props)
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
-        const { classes } = this.props
-        return (
-            <Paper className={classes.root}>
-                <Table className={classes.table}>
-                    <TableHead>
-                        <TableRow>
-                            {this.props.headers.map((header) => (
-                                <TableCell>{header.name}</TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {this.props.data.map((row) => (
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    {row.Names[0].substr(1)}
-                                </TableCell>
-                                <TableCell>{row.State}</TableCell>
-                                <TableCell>{row.Status}</TableCell>
-                                <TableCell>{row.Image}</TableCell>
-                                <TableCell>
-                                    {row.Ports[0].IP}, {row.Ports[0].PrivatePort} -> {row.Ports[0].PublicPort}
-                                </TableCell>
-                                <TableCell>
-                                    <Button variant="contained" color={row.State == 'running' ? 'secondary' : green}>
-                                        <Stop />
-                                    </Button>
-                                    <Button variant="contained">
-                                        <Delete />
-                                    </Button>
-                                    <Button variant="contained" color="primary">
-                                        <AutoRenew />
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Paper>
-        )
-    }
+	render() {
+		const { classes } = this.props;
+		return (
+			<Paper className={classes.root}>
+				<Table className={classes.table}>
+					<TableHead>
+						<TableRow>
+							{this.props.headers.map(header => (
+								<TableCell>{header.name}</TableCell>
+							))}
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{this.props.data.map(row => (
+							<TableRow>
+								<TableCell component="th" scope="row">
+									{row.Names[0].substr(1)}
+								</TableCell>
+								<TableCell>{row.State}</TableCell>
+								<TableCell>{row.Status}</TableCell>
+								<TableCell position="relative">
+									<img src={DockerIcon} position="absolute" bottom={0} height={24} width={24} />
+									{row.Image}
+								</TableCell>
+								<TableCell>
+									{row.Ports[0].IP}, {row.Ports[0].PrivatePort} -> {row.Ports[0].PublicPort}
+								</TableCell>
+								<TableCell>
+									<Button variant="contained" color={row.State == 'running' ? 'secondary' : green}>
+										<Stop />
+									</Button>
+									<Button variant="contained">
+										<Delete />
+									</Button>
+									<Button variant="contained" color="primary">
+										<AutoRenew />
+									</Button>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</Paper>
+		);
+	}
 }
 
 ContainersTable.propTypes = {
-    classes: PropTypes.object.isRequired,
-}
+	classes: PropTypes.object.isRequired
+};
 
-export default withStyles(styles)(ContainersTable)
+export default withStyles(styles)(ContainersTable);
--- src/components/elements/imagesTable.js
+++ src/components/elements/imagesTable.js
@@ -1,64 +1,71 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
-import classnames from 'classnames'
-import moment from 'moment'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import classnames from 'classnames';
+import moment from 'moment';
 
-const styles = (theme) => ({
-    root: {
-        width: '100%',
-        overflowX: 'auto',
-        marginTop: 16,
-    },
-    table: {
-        minWidth: 700,
-    },
-})
+const styles = theme => ({
+	root: {
+		width: '100%',
+		overflowX: 'auto',
+		marginTop: 16
+	},
+	table: {
+		minWidth: 700
+	}
+});
 
 class ImagesTable extends Component {
-    constructor(props) {
-        super(props)
-    }
+	constructor(props) {
+		super(props);
+		this.bytesToSize = this.bytesToSize.bind(this);
+	}
 
-    render() {
-        const { classes } = this.props
-        console.log('moment', moment(1536303210))
-        return (
-            <Paper className={classes.root}>
-                <Table className={classes.table}>
-                    <TableHead>
-                        <TableRow>
-                            {this.props.headers.map((header) => (
-                                <TableCell>{header.name}</TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {this.props.data.map((row) => (
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    {row.RepoTags[0]}
-                                </TableCell>
-                                <TableCell>{row.Id}</TableCell>
-                                <TableCell>{moment.duration(row.Created, 'milliseconds').days()}</TableCell>
-                                <TableCell>{row.Size}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Paper>
-        )
-    }
+	bytesToSize(bytes) {
+		var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+		if (bytes == 0) return '0 Byte';
+		var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+		return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+	}
+
+	render() {
+		const { classes } = this.props;
+		return (
+			<Paper className={classes.root}>
+				<Table className={classes.table}>
+					<TableHead>
+						<TableRow>
+							{this.props.headers.map(header => (
+								<TableCell>{header.name}</TableCell>
+							))}
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{this.props.data.map(row => (
+							<TableRow>
+								<TableCell component="th" scope="row">
+									{row.RepoTags[0]}
+								</TableCell>
+								<TableCell>{row.Id}</TableCell>
+								<TableCell>{moment.duration(row.Created, 'milliseconds').days()}</TableCell>
+								<TableCell>{this.bytesToSize(row.Size)}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</Paper>
+		);
+	}
 }
 
 ImagesTable.propTypes = {
-    classes: PropTypes.object.isRequired,
-}
+	classes: PropTypes.object.isRequired
+};
 
-export default withStyles(styles)(ImagesTable)
+export default withStyles(styles)(ImagesTable);
