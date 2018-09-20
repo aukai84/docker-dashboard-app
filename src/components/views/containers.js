f5238e5c2c05ca3b969c5803d0a71b5433f57812
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-19 21:22:37 -1000

    containers table working with icons

--- src/components/views/containers.js
+++ src/components/views/containers.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react'
-import Typography from '@material-ui/core/Typography'
-import DashboardTable from '../elements/dashboardTable.js'
-import Grid from '@material-ui/core/Grid'
-import { withStyles } from '@material-ui/core'
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import ContainersTable from '../elements/containersTable.js';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core';
 
 class Containers extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        const headers = [
-            { name: 'Name', value: 'Name' },
-            { name: 'Status', value: 'Status' },
-            { name: 'Image', value: 'Image' },
-            { name: 'IP/PORT', value: 'IP/PORT' },
-        ]
-        return (
-            <div>
-                <Typography variant="headline" component="h1">
-                    Containers
-                </Typography>
-                <DashboardTable headers={headers} />
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		console.log('container props', this.props);
+		const headers = [
+			{ name: 'Name', value: 'Names[0]' },
+			{ name: 'State', value: 'State' },
+			{ name: 'Status', value: 'Status' },
+			{ name: 'Image', value: 'Image' },
+			{ name: 'IP/PORT', value: 'Ports[0]' },
+			{ name: '', value: 'actions' }
+		];
+		return (
+			<div>
+				<Typography variant="headline" component="h1">
+					Containers
+				</Typography>
+				<ContainersTable data={this.props.containers} headers={headers} />
+			</div>
+		);
+	}
 }
 
-export default Containers
+export default Containers;
--- src/containers/mainView.js
+++ src/containers/mainView.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Dashboard from '../components/views/dashboard.js'
-import Containers from '../components/views/containers.js'
-import Images from '../components/views/images.js'
-import Usage from '../components/views/usage.js'
-import Networks from '../components/views/networks.js'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from '../components/views/dashboard.js';
+import Containers from '../components/views/containers.js';
+import Images from '../components/views/images.js';
+import Usage from '../components/views/usage.js';
+import Networks from '../components/views/networks.js';
 
 class MainView extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (
-            <Switch>
-                <Route path="/" exact component={Dashboard} />
-                <Route path="/containers" component={Containers} />
-                <Route path="/images" exact component={Images} />
-                <Route path="/usage" exact component={Usage} />
-                <Route path="/networks" exact component={Networks} />
-            </Switch>
-        )
-    }
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<Switch>
+				<Route path="/" exact component={Dashboard} />
+				<Route path="/containers" render={() => <Containers containers={this.props.containers} />} />
+				<Route path="/images" render={() => <Images images={this.props.images} />} />
+				<Route path="/usage" component={Usage} />
+				<Route path="/networks" component={Networks} />
+			</Switch>
+		);
+	}
 }
 
-export default MainView
+export default MainView;
