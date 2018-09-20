bebe0f2b2bfffad397d4e44bcf2be6ac66019bdf
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-19 16:04:46 -1000

    prettier fixes

--- src/components/elements/containersTable.js
+++ src/components/elements/containersTable.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 
 class ContainersTable extends Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props)
+    }
 }
 
-export default ContainersTable;
+export default ContainersTable
--- src/components/views/containers.js
+++ src/components/views/containers.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core';
+import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core'
 
 class Containers extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div>
-				<Typography variant="headline" component="h1">
-					Containers
-				</Typography>
-			</div>
-		);
-	}
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <Typography variant="headline" component="h1">
+                    Containers
+                </Typography>
+            </div>
+        )
+    }
 }
 
-export default Containers;
+export default Containers
