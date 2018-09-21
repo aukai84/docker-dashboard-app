52df5805019ff2a64cc3cb1fd400eb0afd5dab0d
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-09-21 08:56:07 -1000

    updating package... not building correctly

--- server.js
+++ server.js
@@ -1,86 +1,86 @@
 #!/usr/bin/env node
 
-let express = require('express')
-let path = require('path')
+import express, { static } from 'express'
+import { join } from 'path'
 let app = express()
 let server = require('http').Server(app)
 let io = require('socket.io')(server)
-let cors = require('cors')
+import cors from 'cors'
 const PORT = process.env.PORT || 8000
 
-let docker = require('./dockerapi')
+import { getContainer, listContainers, listImages, listNetworks } from './dockerapi'
 
 app.use(cors())
-app.use(express.static('./dist'))
+app.use(static('./dist'))
 
-app.get('/*', (req, res, next) => res.sendFile(path.join(__dirname, 'dist/index.html')))
+app.get('/*', (req, res, next) => res.sendFile(join(__dirname, 'dist/index.html')))
 
 server.listen(PORT, () => {
-    console.log('Docker Dashboard Server running on port ', PORT)
+	console.log('Docker Dashboard Server running on port ', PORT)
 })
 
-io.on('connection', (socket) => {
-    socket.on('containers.list', () => {
-        refreshContainers()
-    })
-    socket.on('images.list', () => {
-        refreshImages()
-    })
-    socket.on('networks.list', () => {
-        refreshNetworks()
-    })
-    socket.on('cpu.usage', () => {
-        refreshCpuUsage()
-    })
-    socket.on('container.start', (args) => {
-        const container = docker.getContainer(args.id)
-        if (container) {
-            container.start((err, data) => refreshContainers())
-        }
-    })
-    socket.on('container.stop', (args) => {
-        const container = docker.getContainer(args.id)
-        if (container) {
-            container.stop((err, data) => refreshContainers())
-        }
-    })
+io.on('connection', socket => {
+	socket.on('containers.list', () => {
+		refreshContainers()
+	})
+	socket.on('images.list', () => {
+		refreshImages()
+	})
+	socket.on('networks.list', () => {
+		refreshNetworks()
+	})
+	socket.on('cpu.usage', () => {
+		refreshCpuUsage()
+	})
+	socket.on('container.start', args => {
+		const container = getContainer(args.id)
+		if (container) {
+			container.start((err, data) => refreshContainers())
+		}
+	})
+	socket.on('container.stop', args => {
+		const container = getContainer(args.id)
+		if (container) {
+			container.stop((err, data) => refreshContainers())
+		}
+	})
 })
 
 function refreshContainers() {
-    docker.listContainers({ all: true }, (err, containers) => {
-        io.emit('containers.list', containers)
-    })
+	listContainers({ all: true }, (err, containers) => {
+		io.emit('containers.list', containers)
+	})
 }
 function refreshImages() {
-    docker.listImages({ all: false }, (err, images) => {
-        io.emit('images.list', images)
-    })
+	listImages({ all: false }, (err, images) => {
+		io.emit('images.list', images)
+	})
 }
 function refreshNetworks() {
-    docker.listNetworks({ all: true }, (err, networks) => {
-        io.emit('networks.list', networks)
-    })
+	listNetworks({ all: true }, (err, networks) => {
+		io.emit('networks.list', networks)
+	})
 }
 
 function refreshCpuUsage() {
-    function calculatePercentage(cpu, preCpu, system, preSystem) {
-        let cpuDelta = cpu - preCpu
-        let systemDelta = system - preSystem
-        let usage = (cpuDelta / systemDelta) * 100
-        return usage
-    }
-    docker.listContainers({ all: true }, (err, containers) => {
-        if (containers) {
-            let dockerContainer = docker.getContainer(containers[0].Id)
-            dockerContainer.stats({ stream: false }, (err, res) => {
-                let cpuStats = res.cpu_stats.cpu_usage.total_usage
-                let preCpuStats = res.precpu_stats.cpu_usage.total_usage
-                let systemCpuStats = res.cpu_stats.system_cpu_usage
-                let systemPreCpuStats = res.precpu_stats.system_cpu_usage
-                io.emit('cpu.usage', calculatePercentage(cpuStats, preCpuStats, systemCpuStats, systemPreCpuStats))
-            })
-        }
-        /*containers.forEach((container) => {
+	function calculatePercentage(cpu, preCpu, system, preSystem) {
+		let cpuDelta = cpu - preCpu
+		let systemDelta = system - preSystem
+		let usage = (cpuDelta / systemDelta) * 100
+		return usage
+	}
+	listContainers({ all: true }, (err, containers) => {
+		if (containers) {
+			let dockerContainer = getContainer(containers[0].Id)
+			dockerContainer.stats({ stream: false }, (err, res) => {
+				let cpuStats = res.cpu_stats.cpu_usage.total_usage
+				let preCpuStats = res.precpu_stats.cpu_usage.total_usage
+				let systemCpuStats = res.cpu_stats.system_cpu_usage
+				let systemPreCpuStats = res.precpu_stats.system_cpu_usage
+				io.emit('cpu.usage', calculatePercentage(cpuStats, preCpuStats, systemCpuStats, systemPreCpuStats))
+			})
+		}
+		/*containers.forEach((container) => {
             let dockerContainer = docker.getContainer(container.Id)
             dockerContainer.stats({ stream: false }, (err, res) => {
                 cpuStats += res.cpu_stats.cpu_usage.total_usage
@@ -89,7 +89,7 @@ function refreshCpuUsage() {
                 systemPreCpuStats += res.precpu_stats.system_cpu_usage
             })
         })*/
-    })
+	})
 }
 
 setInterval(refreshCpuUsage, 2000)
